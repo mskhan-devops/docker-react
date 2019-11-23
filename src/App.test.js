2ecff8a65172470f4e7f4d26af9b395b16d1e4dc
Muhammad Siddique <mskhan@etilizepak.com>
2019-11-23 11:54:43 +0500

    added travis file

--- src/App.test.js
+++ src/App.test.js
@@ -6,10 +6,3 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
-
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
-
